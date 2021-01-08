24cd02e17934fa8f35332002cae20de71dc542bb
unknown <Nikolaua36@gmail.com>
2021-01-09 00:00:48 +0200

    first commit

---
+++ 471 Stepanenko Laba 7/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _471_Stepanenko_Laba_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // helpProvider1
+            // 
+            this.helpProvider1.HelpNamespace = "C:\\Users\\USER\\source\\repos\\471 Stepanenko Laba 7\\471 Stepanenko Laba 7\\help.html";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ви можете натиснути F1 для отримання довідки";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.helpProvider1.SetShowHelp(this, true);
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ 471 Stepanenko Laba 7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _471_Stepanenko_Laba_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ToolStripMenuItem fileItem = new ToolStripMenuItem("Файл");
+
+            fileItem.DropDownItems.Add("Закрити");
+            fileItem.DropDownItems[0].Click += aboutItem_Click;
+            menuStrip1.Items.Add(fileItem);
+
+            ToolStripMenuItem aboutItem = new ToolStripMenuItem("О программе");
+            aboutItem.DropDownItems.Add("Автор: Степаненко М.І. 471");
+            menuStrip1.Items.Add(aboutItem);
+
+            ToolStripMenuItem helpItem = new ToolStripMenuItem("Довідка");
+            helpItem.Click += helpItem_Click;
+            menuStrip1.Items.Add(helpItem);
+        }
+        private void aboutItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void helpItem_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, @"C:\Users\USER\source\repos\471 Stepanenko Laba 7\471 Stepanenko Laba 7\help.html");
+        }
+    }
+}
